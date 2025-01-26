61fc12e3b1392989b5e9bcfabfd47c08d8be7c78
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-01-26 19:50:55 +0100

    ci: edit chart editor script

--- update-helm-version.sh
+++ update-helm-version.sh
@@ -2,4 +2,5 @@
 NEW_VERSION=$1
 CHART_YAML="./Chart.yaml"
 sed -i "s/^version:.*/version: \"$NEW_VERSION\"/" "$CHART_YAML"
+sed -i "s/^appVersion:.*/appVersion: \"$NEW_VERSION\"/" "$CHART_YAML"
 git add "$CHART_YAML"
