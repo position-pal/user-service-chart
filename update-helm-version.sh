058edf90250e66538ab52ae33789e749b3342720
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-01-26 18:13:26 +0100

    feat: prepare chart env

--- update-helm-version.sh
+++ update-helm-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 NEW_VERSION=$1
 CHART_YAML="./Chart.yaml"
-sed -i "s/^appVersion:.*/appVersion: \"$NEW_VERSION\"/" "$CHART_YAML"
+sed -i "s/^version:.*/version: \"$NEW_VERSION\"/" "$CHART_YAML"
 git add "$CHART_YAML"
